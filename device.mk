35373c7e7c4f416147e3cc41ebe6217d815c93aa
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-10-06 17:51:00 +0530

    oneplus3: Cleanup init.qcom.bt.sh
    * Remove bluez support
    * Set properties in device.mk
    * Bring closer to AOSP devices' script
    
    Change-Id: I6eeb69487b02070a84bc2958fb8a939a684ab915

--- device.mk
+++ device.mk
@@ -133,6 +133,19 @@ PRODUCT_PACKAGES += \
     libantradio \
     antradio_app
 
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bluetooth.dun=true \
+    ro.bluetooth.hfp.ver=1.6 \
+    ro.bluetooth.sap=true \
+    ro.qualcomm.bluetooth.ftp=true \
+    ro.qualcomm.bluetooth.hfp=true \
+    ro.qualcomm.bluetooth.hsp=true \
+    ro.qualcomm.bluetooth.map=true \
+    ro.qualcomm.bluetooth.nap=true \
+    ro.qualcomm.bluetooth.opp=true \
+    ro.qualcomm.bluetooth.pbap=true
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
