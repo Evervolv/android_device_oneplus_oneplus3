502417b33a0e2d06a7ef13cdffe209ed584f32b7
Nick Reuter <nreuter85@gmail.com>
2016-10-28 04:10:12 -0500

    op3: cm -> ev
    Change-Id: I2f91bc02bc4bb4ab1dc3e96094631464f9b969bd

--- ev.mk
+++ ev.mk
@@ -12,34 +12,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Call this first so apn list is actually copied
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/apns.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from oneplus3 device
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit some common evervolv stuff.
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common_full_phone.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Pull all dictionaries
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/dictionaries/intl.mk)
 
-PRODUCT_NAME := cm_oneplus3
+# Bootanimation
+BOOT_ANIMATION_SIZE := 1080p
+
+PRODUCT_NAME := ev_oneplus3
 PRODUCT_DEVICE := oneplus3
-PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := OnePlus3
+PRODUCT_MANUFACTURER := OnePlus
 
-PRODUCT_GMS_CLIENTID_BASE := android-oneplus
-
-TARGET_VENDOR_PRODUCT_NAME := OnePlus3
-TARGET_VENDOR_DEVICE_NAME := OnePlus3
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus3 PRODUCT_NAME=OnePlus3
+PRODUCT_CODENAME := Plures
+PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your OnePlus 3\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=OnePlus3 \
+    PRODUCT_NAME=OnePlus3 \
     BUILD_FINGERPRINT=OnePlus/OnePlus3/OnePlus3:6.0.1/MMB29M/362280:user/release-keys \
     PRIVATE_BUILD_DESC="OnePlus3-user 6.0.1 MMB29M 24 dev-keys"
 
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
-
-TARGET_VENDOR := oneplus
