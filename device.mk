dfd1bbbc3fe7b6360240018a815976564b538243
Nick Reuter <nreuter85@gmail.com>
2016-11-10 15:56:09 -0600

    op3: initial setup to support aosp
    Change-Id: I17da1754471e38c8bd8e2c278ba595d5acaa8f4d

--- device.mk
+++ device.mk
@@ -21,11 +21,6 @@
 # definition file).
 #
 
-# Device was launched with M
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
-
-$(call inherit-product, vendor/oneplus/oneplus3/oneplus3-vendor.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
--- lineage.mk
+++ lineage.mk
@@ -13,12 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit from oneplus3 device
-$(call inherit-product, device/oneplus/oneplus3/device.mk)
+$(call inherit-product, device/oneplus/oneplus3/aosp_oneplus3.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
