b227bd500ce70472358625bc3ceda86df78e2f9e
Bruno Martins <bgcngm@gmail.com>
2017-01-10 22:17:02 +0000

    op3: Set device bring up year
     * Specifying INITIAL_COPYRIGHT_YEAR ensures that the correct copyrights
       are included in vendor makefiles
    
    Change-Id: I2aa73e6f91fbf08e20873605fcc6e1c3214c2204

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -21,6 +21,8 @@ set -e
 DEVICE=oneplus3
 VENDOR=oneplus
 
+INITIAL_COPYRIGHT_YEAR=2016
+
 # Load extractutils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
