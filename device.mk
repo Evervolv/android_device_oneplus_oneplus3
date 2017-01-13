dc5a93ac472175051309aedaf788ce687c61e970
Myles Watson <mylesgw@google.com>
2017-01-13 11:47:17 -0800

    Build the Bluetooth HAL for Oneplus3
    Test: VTS test passes on Oneplus3, Bluetooth starts/stops
    Change-Id: Ib1a1849c060c39e88c0219cb8421e3426cd3e7ac

--- device.mk
+++ device.mk
@@ -165,7 +165,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 PRODUCT_PROPERTY_OVERRIDES += \
     qcom.bluetooth.soc=rome
