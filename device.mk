2622d6d80e47d6c66096ceb2377606629e6440b6
Alexander Martinz <eviscerationls@gmail.com>
2017-01-31 15:16:06 +0100

    op3: s6e3fa5: add QDCM configuration
    The different display modes are not functional yet, but we want the luts.
    
    Though the display mode names do not match our expected mapping, so the
    profile configuration preference does not show up in settings, means no
    harm done.
    
    Change-Id: Ic43cb3c5a83a9e17415797c8aed1d8e7e17dd5ca
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- device.mk
+++ device.mk
@@ -177,7 +177,8 @@ PRODUCT_PACKAGES += \
 # Display calibration
 PRODUCT_PACKAGES += \
     FOSSConfig.xml \
-    qdcm_calib_data_samsung_s6e3fa3_1080p_cmd_mode_dsi_panel.xml
+    qdcm_calib_data_samsung_s6e3fa3_1080p_cmd_mode_dsi_panel.xml \
+    qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
