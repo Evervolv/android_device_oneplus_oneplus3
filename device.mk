4201e26b5653e8d89db4006380ff98618879f8fe
Wei Wang <wvw@google.com>
2017-02-07 14:31:42 -0800

    op3: Split VR thermal config into separate file
    1) move thermal config into vendor/ partition
    2) VR hal will set properties to point to correct thermal-engine config
       and call init to restart thermal-engine with updated config
    3) once thermal-engine died, init will restart thermal-engine with current
       active thermal config
    
    Bug: 33275316
    Test: on marlin with VR mode on/off
    Change-Id: I31c1f573c6bb589b297e8706ad0667c280d7b417

--- device.mk
+++ device.mk
@@ -413,7 +413,8 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+    $(LOCAL_PATH)/configs/thermal-engine-vr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-vr.conf
 
 # Vibrator
 PRODUCT_PACKAGES += \
