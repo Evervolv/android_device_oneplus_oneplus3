aed71e4f9d2cfff770c59c548227d57f81ac3aca
Vinay Verma <vvinay@codeaurora.org>
2017-02-20 09:51:31 +0530

    op3: Enable Lights HAL compilation for msm8996
    Lights Impl HAL compilation is enabled to support screen
    turn on/off through power key.
    
    Change-Id: I5b6330de55b3eba36da0d14d225283ee319509de

--- device.mk
+++ device.mk
@@ -248,7 +248,8 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    lights.msm8996
+    lights.msm8996 \
+    android.hardware.light@2.0-impl
 
 # LLVM
 $(call inherit-product-if-exists, external/llvm/llvm-select.mk)
