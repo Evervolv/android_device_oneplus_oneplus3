2c6bd4e13a2d27e1c50d64c066f53eedca3f11bb
dianlujitao <dianlujitao@lineageos.org>
2017-03-03 21:47:42 +0800

    op3: Update for stock 7.1.1 blobs
    Change-Id: I27e66c8c21cf0a22f2d6b22b8527e6d223836b55

--- device.mk
+++ device.mk
@@ -76,11 +76,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.first_api_level=23
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapminfree=4m \
     dalvik.vm.heapmaxfree=8m
 
 PRODUCT_PROPERTY_OVERRIDES += \
