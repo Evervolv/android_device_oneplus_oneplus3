18c96a9cc52cebd85a2f85a28a45f03938a98e07
Haynes Mathew George <hgeorge@codeaurora.org>
2018-10-07 18:41:35 -0500

    system.prop: Set default period sizes
    Set new default period sizes for ull and low-latency-record
    audio usecases
    
    Bug: 29822375
    
    Change-Id: I75437818283f70dd285180f121bbb2e2b120e06c

--- device.mk
+++ device.mk
@@ -169,7 +169,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.hw.aac.encoder=true \
     persist.vendor.bt.enable.splita2dp=false \
     ro.config.media_vol_steps=25 \
-    ro.config.vc_call_vol_steps=7
+    ro.config.vc_call_vol_steps=7 \
+    audio_hal.period_multiplier=2
 
 # ANT+
 PRODUCT_PACKAGES += \
