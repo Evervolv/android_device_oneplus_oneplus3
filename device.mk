e1ba5697315d8bfe76d5006e95ac3dc223ffd8a2
dianlujitao <dianlujitao@lineageos.org>
2018-10-29 20:38:20 +0800

    op3: Update system properties from LA.UM.7.5.r1-03100-8x96.0
    Change-Id: Ib2684067ececef3527d386daae65708117eb4518

--- device.mk
+++ device.mk
@@ -85,12 +85,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
-    persist.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.sib16_support=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     sys.vendor.shutdown.waittime=500 \
-    ro.build.shutdown_timeout=0 \
     ro.frp.pst=/dev/block/bootdevice/by-name/config \
     persist.radio.multisim.config=dsds \
     debug.hwui.use_buffer_age=false
