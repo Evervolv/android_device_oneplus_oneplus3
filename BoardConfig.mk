73ad6b5d74032f70e10e90600f456a12a8c5d01e
dianlujitao <dianlujitao@lineageos.org>
2018-10-30 19:26:48 +0800

    op3: Update manifest and FCM from LA.UM.7.5.r1-03100-8x96.0
    Change-Id: Ie55451ac1faaddd848c9137da76beff9f83a2c9a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,6 +174,7 @@ TARGET_PROVIDES_KEYMASTER := true
 TARGET_PROVIDES_LIBLIGHT := true
 
 # Mainfest
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(PLATFORM_PATH)/configs/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(PLATFORM_PATH)/configs/manifest.xml
 DEVICE_MATRIX_FILE   := $(PLATFORM_PATH)/configs/compatibility_matrix.xml
 
