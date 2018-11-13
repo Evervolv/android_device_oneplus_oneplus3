dff690aa87d55f1be18176baebd80fa435327edb
dianlujitao <dianlujitao@lineageos.org>
2018-11-13 15:32:07 +0800

    op3: Kang GPS blobs from B2N_sprout
    Change-Id: Ia1646ae0b8158c1b312221788c08427a754f3a36

--- extract-files.sh
+++ extract-files.sh
@@ -67,10 +67,6 @@ extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
 COMMON_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 
-patchelf --replace-needed android.hardware.gnss@1.0.so android.hardware.gnss@1.0-v27.so $COMMON_BLOB_ROOT/lib64/vendor.qti.gnss@1.0.so
-patchelf --replace-needed android.hardware.gnss@1.0.so android.hardware.gnss@1.0-v27.so $COMMON_BLOB_ROOT/vendor/lib64/vendor.qti.gnss@1.0_vendor.so
-patchelf --replace-needed android.frameworks.sensorservice@1.0.so android.frameworks.sensorservice@1.0-v27.so $COMMON_BLOB_ROOT/vendor/bin/slim_daemon
-
 CAMERA_HAL="$COMMON_BLOB_ROOT"/vendor/lib/hw/camera.msm8996.so
 
 sed -i \
