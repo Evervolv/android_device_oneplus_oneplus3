f452835e9a51a3c64359e7f34a3a7028847349bd
dianlujitao <dianlujitao@lineageos.org>
2019-03-15 17:58:23 +0800

    op3: Remove SE from manifest
    Change-Id: I953ece7834678d4f2ba79baebd09b480ed4a0f49

--- device.mk
+++ device.mk
@@ -431,7 +431,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.2 \
+    android.hardware.radio@1.1 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
