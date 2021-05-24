1396fefbfb3c9fced4b965306e7ce5e6b4c01280
Nick Reuter <nreuter85@gmail.com>
2021-05-23 19:44:13 -0500

    op3: remove pocketmode
     Service is moving to ConfigPanel
    
    Change-Id: I7735d66bddb2b2345fc40ffb19e0e9197c38a032

--- device.mk
+++ device.mk
@@ -281,7 +281,6 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    OneplusPocketMode \
     android.hardware.biometrics.fingerprint@2.1-service \
     vendor.oneplus.fingerprint.extension@1.0 \
     vendor.oneplus.hardware.display@1.0
