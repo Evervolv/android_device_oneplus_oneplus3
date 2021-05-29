6bc6b7caeb28bff43edd70360aad891d4d982a10
Nick Reuter <nreuter85@gmail.com>
2021-05-29 00:50:53 -0500

    op3: move KeyHandler out of ConfigPanel
    Change-Id: I0572754a74e10d6ba8098dc3bb5d9703f89d6d0c

--- device.mk
+++ device.mk
@@ -389,6 +389,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.nfc_nci=nqx.default \
     vendor.qti.nfc.chipid=0x51
 
+# Notification slider
+PRODUCT_PACKAGES += \
+    KeyHandler
+
 # OEM Unlock reporting
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.oem_unlock_supported=1
