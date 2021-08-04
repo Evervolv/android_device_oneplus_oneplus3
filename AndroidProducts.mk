LineageOS/android_device_moto_shamu

shamu: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -1,5 +1,6 @@
 #
 # Copyright 2015 The Android Open Source Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,4 +17,9 @@
 
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/lineage_shamu.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_shamu-user \
+    lineage_shamu-userdebug \
+    lineage_shamu-eng
 

Evervolv/android_device_oneplus_oneplus3

AndroidProducts.mk
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/ev_oneplus3.mk
+
+COMMON_LUNCH_CHOICES := \
+    ev_oneplus3-user \
+    ev_oneplus3-userdebug \
+    ev_oneplus3-eng